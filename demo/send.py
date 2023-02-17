d136234e0235fd74109d3342a536686ae81e9a60
Brian Lim <52195373+lsh950919@users.noreply.github.com>
2023-02-17 12:59:43 -0500

    Update send.py

--- demo/send.py
+++ demo/send.py
@@ -6,45 +6,23 @@ import audioop
 import datetime
 import math
 import json
+import argparse
 from pathlib import Path
 
-def decibel( data, width ):
+def decibel(data, width):
     rms = audioop.rms(data, width)
     if rms == 0:
         return 0
     else:
         return 20 * math.log(rms, 10)
 
-def rescaled_decibel( data, width ):
+def rescaled_decibel(data, width):
     rms = audioop.rms(data, width)
     if rms == 0:
         return 0
     else:
         decibel = 20 * math.log(rms, 10)
         return math.tanh( (decibel - 75) * 0.05 )
-
-
-chunk = 4410 #1024  # Record in chunks of 1024 samples
-sample_format = pyaudio.paInt16  # 16 bits per sample
-channels = 1
-fs = 44100  # Record at 44100 samples per second
-# seconds = 10
-
-
-# Create an interface to PortAudio
-p = pyaudio.PyAudio()
-
-# Open a .Stream object to get mic input
-stream = p.open(format = sample_format,
-                channels = channels,
-                rate = fs,
-                frames_per_buffer = chunk,
-                input = True)
-
-sampwidth = p.get_sample_size(sample_format)
-rate = fs
-
-
 
 async def handler(websocket):
     i = 0
@@ -63,14 +41,37 @@ async def handler(websocket):
                 'db_rescale': rescaled_decibel_value}
 
         res = json.dumps(temp)
-
         await websocket.send(res)
 
-
-async def main():
-    async with websockets.serve(handler, "172.26.174.12", 8001, ping_interval=None):
+async def main(ip_address):
+    async with websockets.serve(handler, ip_address, 8001, ping_interval=None):
         await asyncio.Future()  # run forever
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-ip', '--ip_address', type=str)
+    parser.add_argument('--chunk', type=int, default=4410)
+    parser.add_arugment('--channel', type=int, default=1)
+    parser.add_argument('--sample_rate', type=int, default=44100)
+    args = parser.parse_args()
+
+    chunk = args.chunk #1024  # Record in chunks of 1024 samples
+    sample_format = pyaudio.paInt16  # 16 bits per sample
+    channels = args.channel
+    fs = args.sampel_rate # Record at 44100 samples per second
+
+    # Create an interface to PortAudio
+    p = pyaudio.PyAudio()
+    
+    # Open a .Stream object to get mic input
+    stream = p.open(format = sample_format,
+                    channels = channels,
+                    rate = fs,
+                    frames_per_buffer = chunk,
+                    input = True)
+    
+    sampwidth = p.get_sample_size(sample_format)
+    rate = fs
+    
+    asyncio.run(main(args.ip_address))
